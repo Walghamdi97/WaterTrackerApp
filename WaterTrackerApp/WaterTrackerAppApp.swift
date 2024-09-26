1c8009619df9e1e5062aef30c39241042ca0dad7
Walghamdi97 <wejgh1997@gmail.com>
2024-09-26 11:46:17 +0300

    Initial Commit

---
+++ WaterTrackerApp/WaterTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerAppApp.swift
+//  WaterTrackerApp
+//
+//  Created by Wejdan Alghamdi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
