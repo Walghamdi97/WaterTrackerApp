adb5c2ff5bf16e6a43afc2bf5da953a0913792f7
Walghamdi97 <wejgh1997@gmail.com>
2024-09-26 12:58:07 +0300

    New Updates

--- WaterTrackerApp/ContentView.swift
+++ WaterTrackerApp/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var off = false;
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("Water Tracker 💦")
+                .fontWeight(.bold)
+                
+            }
             
-            Text("I used app to try the command")
+            Toggle(isOn: $off) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple health")/*@END_MENU_TOKEN@*/
+            }
+           
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Cups to drink per day")/*@END_MENU_TOKEN@*/
+            }
+            
+            Button(/*@START_MENU_TOKEN@*/"Continue"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
         }
         .padding()
     }
